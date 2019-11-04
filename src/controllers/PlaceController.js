9cfdad31240e12f3799e2e2a33e8737759a67a45
ericrprates <ericrprates@gmail.com>
2019-11-04 20:22:22 -0300

    fixed place searchs

--- src/controllers/PlaceController.js
+++ src/controllers/PlaceController.js
@@ -3,14 +3,53 @@ const axios = require("axios");
 
 class PlaceController {
   async nearbySearch(req, res) {
-    const { location, pagetoken, keyword } = req.query;
+    const { location, pagetoken } = req.query;
     try {
-      const resp = await axios.get(
-        !pagetoken
-          ? `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=5000&type=restaurant&key=${process.env.GOOGLE_PLACES_KEY}`
-          : `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&pagetoken=${pagetoken}&radius=5000&type=restaurant&key=${process.env.GOOGLE_PLACES_KEY}`
-      );
-      return res.send(resp.data);
+      const lat = location.split(",")[0].split(".")[0];
+      const long = location.split(",")[1].split(".")[0];
+      if (!pagetoken) {
+        const places = await Place.find({
+          latitude: lat,
+          longitude: long
+        }).lean();
+        if (places.length === 0) {
+          const resp = await axios.get(
+            `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=5000&type=restaurant&key=${process.env.GOOGLE_PLACES_KEY}`
+          );
+          if (resp.data.status === "OK") {
+            for (let index = 0; index < resp.data.results.length; index++) {
+              await Place.create({
+                ...resp.data.results[index],
+                latitude: lat,
+                longitude: long
+              });
+            }
+          }
+          return res.send(resp.data);
+        } else {
+          return res.send(places);
+        }
+      } else {
+        const resp = await axios.get(
+          `https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${pagetoken}&key=${process.env.GOOGLE_PLACES_KEY}`
+        );
+        if (resp.data.status === "OK") {
+          for (let index = 0; index < resp.data.results.length; index++) {
+            const local = await Place.findOne({
+              place_id: resp.data.results[index].place_id
+            }).lean();
+            if (!local) {
+              console.log("criou");
+              await Place.create({
+                ...resp.data.results[index],
+                latitude: lat,
+                longitude: long
+              });
+            }
+          }
+        }
+        return res.send(resp.data);
+      }
     } catch (e) {
       console.log(e);
       return res.send(false);
